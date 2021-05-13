6d170d7f806d355a619e410e7a88b23e04ca3caa
blablavlad <blablavlad@mail.ru>
2021-05-13 22:56:19 +0300

    classes update

--- src/carManager/Car.java
+++ src/carManager/Car.java
@@ -5,14 +5,26 @@ public class Car {
     int yearOfProduction;
     int price;
     int weight;
-    String color;
+//    String color;
+    Color color;
 
 //    конструктор
-    public Car(String brand, int yearOfProduction, int price, int weight, String color) {
+    public Car(String brand, int yearOfProduction, int price, int weight, Color color) {
         this.brand = brand;
         this.yearOfProduction = yearOfProduction;
         this.price = price;
         this.weight = weight;
         this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "brand=" + brand +
+                ", yearOfProduction=" + yearOfProduction +
+                ", price=" + price +
+                ", weight=" + weight +
+                ", color=" + color +
+                '}';
     }
 }
--- src/carManager/CarManager.java
+++ src/carManager/CarManager.java
@@ -3,11 +3,14 @@ package carManager;
 public class CarManager {
     public static void main (String[] args){
 
-        Car prius = new Car("Toyota Prius", 2008,15000,1200,"green");
+        Car prius = new Car("Toyota Prius", 2008,15000,1200,Color.GREEN);
 
-        Car renault = new Car("Renault Laguna",2000,9700, 1600, "white");
+        Car renault = new Car("Renault Laguna",2000,9700, 1600, Color.WHITE);
+
+        Car lada = new Car("Lada Granta", 2005, 10000,980,Color.BLUE);
 
         System.out.println(prius);
         System.out.println(renault);
+        System.out.println(lada);
     }
 }
